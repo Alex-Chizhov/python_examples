fb3e69ae2d25db94533e81b41e8eebb311b90603
Alex <alexchizhov90@gmail.com>
2019-01-16 15:27:13 +0300

    Added more complex examples

--- filter_example.py
+++ filter_example.py
@@ -1,3 +1,14 @@
 my_list = [1, 2, 3, 4, 5]
 new_list = list(filter(lambda i: i < 4, my_list))
 print(new_list)
+
+
+my_list = [2, 4, 'abc', 'sdn', None, None, 1, ' ', 'hello']
+new_list = list( filter(lambda i: i!='hello',
+                       filter(lambda i: i!= ' ',
+                              filter(lambda i: i == str(i), my_list))) )
+print(new_list)
+
+my_list = [2, 4, 'abc', 'sdn', None, None, 1, ' ', 'hello']
+new_list = list(filter(lambda i: i!='hello' and i!= ' 'and i == str(i), new_list))
+print(new_list)
